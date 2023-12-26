572f7d313681742f02d7a95d995a979e6c048854
Filippo-Rambelli <108667546+Filippo-Rambelli@users.noreply.github.com>
2023-12-26 16:44:16 +0100

    Update lmc_fromscratch.py

--- lmc_fromscratch.py
+++ lmc_fromscratch.py
@@ -4,6 +4,7 @@ from model_fusion.train import setup_training
 from model_fusion.datasets import DataModuleType
 from model_fusion.models import ModelType
 import torch
+import numpy as np
 
 
 def get_network_parameters(model):
@@ -41,7 +42,7 @@ def combine_parameters(params1,params2,alpha):
     """
     combined_params = []
     for param1, param2 in zip(params1, params2):
-        combined_param = alpha * param1 + (1 - alpha) * param2
+        combined_param = alpha * param1.clone() + (1 - alpha) * param2.clone()
         combined_params.append(torch.nn.Parameter(combined_param))
     return combined_params
 
@@ -64,6 +65,69 @@ def update_network_parameters(model, new_params):
         param.data.copy_(new_param)
 
 
+def compute_loss(model,datamodule):
+
+    model.eval()
+    # Initialize a variable to accumulate the training loss
+    total_loss = 0.0
+
+    # Iterate over the training data and compute predictions
+    for batch in datamodule.train_dataloader():
+       inputs, targets = batch
+
+       # Forward pass
+       predictions = model(inputs)
+
+       # Compute the loss
+       loss = model.loss_module(predictions, targets)
+       total_loss += loss.item()
+
+    # Calculate the average training loss
+    average_loss = total_loss / len(datamodule.train_dataloader())
+    return average_loss
+
+
+def compute_max_and_avg_loss(model1, model2):
+    """
+    Computes the maximum and average loss on the linear path among 2 parent networks
+    """
+    losses = []
+    params1 = get_network_parameters(model1)
+    params2 = get_network_parameters(model2)
+
+    print("params 1", params1[0][0][0])
+    print("params 2", params2[0][0][0])
+
+    # Initialize the fused model as a copy of model1 for each iteration
+    model_fused = model1
+
+    alphas = np.linspace(0.0, 1.0, 11)
+
+    # Iterate over the linear path and compute the maximum loss
+    for alpha in alphas:
+
+        # Compute the parameters on the linear path
+        combined_params = combine_parameters(params1, params2, alpha)
+        print("alpha: ",alpha, "combined params", combined_params[0][0][0])
+
+        # Update the model parameters with the combined parameters
+        update_network_parameters(model_fused, combined_params)
+
+        #sanity check
+        fused_params = get_network_parameters(model_fused)
+        print("alpha: ",alpha, "fused params", fused_params[0][0][0])
+
+        # Compute the loss on the linear path
+        loss = compute_loss(model_fused, datamodule1)
+        losses.append(loss)
+        print(f"Alpha: {alpha:.2f}, Loss: {loss:.2f}")
+    
+    # Compute the maximum and average loss
+    max_loss = np.max(losses)
+    average_loss = np.mean(losses)
+
+    return max_loss, average_loss
+
 
 data_module_type = DataModuleType.MNIST
 
@@ -80,48 +144,33 @@ model1, datamodule1, trainer1 = setup_training('example_experiment', model_type,
 model2, datamodule2, trainer2 = setup_training('example_experiment', model_type, model_hparams, data_module_type, data_module_hparams2, max_epochs=1, wandb_tags=wandb_tags)
 
 
-#fit the parent models and retreive the parameters
+#fit the parent models 
 trainer1.fit(model1, datamodule=datamodule1)
-params1 = get_network_parameters(model1)
-
 trainer2.fit(model2, datamodule=datamodule2)
+
+
+#EXAMPLE: testing alpha = 0.5
+params1 = get_network_parameters(model1)
+print("params 1", params1[0][0][0])
+
 params2 = get_network_parameters(model2)
+print("params 2", params2[0][0][0])
 
-print("PARENT 1 RESULTS:")
-trainer1.test(model1, datamodule=datamodule1)
-print("PARENT 2 RESULTS:")
-trainer2.test(model2, datamodule=datamodule2)
+combined_params = combine_parameters(params1, params2, 0.5)
+print("combined params 0.5", combined_params[0][0][0])
+
+fused_model = model1
+update_network_parameters(fused_model, combined_params)
+fused_params = get_network_parameters(fused_model)
+print("fused params 0.5 ", fused_params[0][0][0])
+
+loss = compute_loss(fused_model, datamodule1)
+print("loss", loss)
 
 
-# Computing the paramters on the linear path between the two parents. Here we do it once, later we will do it in a loop over alpha
-alpha = 0.5  
-combined_params = combine_parameters(params1,params2,alpha)
-
-# Create a new model and set its parameters to the combined parameters
-update_network_parameters(model1, combined_params)
-
-print("COMBINED RESULTS:")
-trainer1.test(model1, datamodule=datamodule1)
-model1.eval()
-
-# Initialize a variable to accumulate the training loss
-total_loss = 0.0
-
-# Iterate over the training data and compute predictions
-for batch in datamodule1.train_dataloader():
-    inputs, targets = batch
-
-    # Forward pass
-    predictions = model1(inputs)
-
-    # Compute the loss
-    loss = model1.loss_module(predictions, targets)
-    total_loss += loss.item()
-
-# Calculate the average training loss
-average_loss = total_loss / len(datamodule1.train_dataloader())
-print(f"Average training loss: {average_loss}")
-
+"""#compute the max and average loss on the linear path
+max_loss, avg_loss = compute_max_and_avg_loss(model1, model2)
+print(f"Maximum loss: {max_loss:.2f}, Average loss: {avg_loss:.2f}")"""
 
 
 wandb.finish()
