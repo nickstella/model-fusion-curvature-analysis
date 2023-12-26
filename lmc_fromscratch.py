6c47ab04f96edb1e75b60ff8f6b33f0facac3e4e
Filippo-Rambelli <108667546+Filippo-Rambelli@users.noreply.github.com>
2023-12-26 09:48:20 +0100

    Update lmc_fromscratch.py

--- lmc_fromscratch.py
+++ lmc_fromscratch.py
@@ -35,10 +35,37 @@ def set_network_parameters(model, params):
     for model_param, input_param in zip(model.parameters(), params):
         model_param.data.copy_(input_param)
 
+def combine_parameters(params1,params2,alpha):
+    """
+    Combine two sets of parameters with a convex combination.
+    """
+    combined_params = []
+    for param1, param2 in zip(params1, params2):
+        combined_param = alpha * param1 + (1 - alpha) * param2
+        combined_params.append(torch.nn.Parameter(combined_param))
+    return combined_params
+
+def update_network_parameters(model, new_params):
+    """
+    Update the parameters of a PyTorch network with new parameters.
+
+    Args:
+        model (torch.nn.Module): The input neural network.
+        new_params (list): A list of parameter tensors to replace the original parameters.
+
+    Returns:
+        None
+    """
+    # Make sure the number of new parameters matches the number of original parameters
+    assert len(list(model.parameters())) == len(new_params), "Number of parameters does not match."
+
+    # Update the model parameters with new_params
+    for param, new_param in zip(model.parameters(), new_params):
+        param.data.copy_(new_param)
+
 
 
 data_module_type = DataModuleType.MNIST
-
 
 #define two different batch size to discrimante among parents
 data_module_hparams1 = {'batch_size': 32}
@@ -53,29 +80,29 @@ model1, datamodule1, trainer1 = setup_training('example_experiment', model_type,
 model2, datamodule2, trainer2 = setup_training('example_experiment', model_type, model_hparams, data_module_type, data_module_hparams2, max_epochs=1, wandb_tags=wandb_tags)
 
 
-#fit the models and retreive the parameters
+#fit the parent models and retreive the parameters
 trainer1.fit(model1, datamodule=datamodule1)
 params1 = get_network_parameters(model1)
-
-print("params1: ", params1[0])
 
 trainer2.fit(model2, datamodule=datamodule2)
 params2 = get_network_parameters(model2)
 
-print("params2: ", params2[0])
+print("PARENT 1 RESULTS:")
+trainer1.test(model1, datamodule=datamodule1)
+print("PARENT 2 RESULTS:")
+trainer2.test(model2, datamodule=datamodule2)
 
 
-# Create a convex combination of the parameters, here we simply do it once. Later we will let alpha vary
+# Computing the paramters on the linear path between the two parents. Here we do it once, later we will do it in a loop over alpha
 alpha = 0.5  
-combined_params = []
-for param1, param2 in zip(params1, params2):
-    combined_param = alpha * param1 + (1 - alpha) * param2
-    combined_params.append(torch.nn.Parameter(combined_param))
+combined_params = combine_parameters(params1,params2,alpha)
 
+# Create a new model and set its parameters to the combined parameters
+update_network_parameters(model1, combined_params)
 
-#check
-print("combined params",combined_params[0])
-#indeed they correspond to the average here
+print("COMBINED RESULTS:")
+trainer1.test(model1, datamodule=datamodule1)
+
 
 wandb.finish()
 
