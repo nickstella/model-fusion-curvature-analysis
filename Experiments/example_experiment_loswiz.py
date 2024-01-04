0c82aa85a562e3d80d9d71fa1c6ff920597c8a44
Filippo-Rambelli <108667546+Filippo-Rambelli@users.noreply.github.com>
2024-01-04 16:49:22 +0100

    Update example_experiment_loswiz.py

--- Experiments/example_experiment_loswiz.py
+++ Experiments/example_experiment_loswiz.py
@@ -7,14 +7,14 @@ from model_fusion.config import BASE_DATA_DIR
 import torch
 
 
-def run_experiment(min_epochs=1, max_epochs=2, batch_size=256):
+def run_experiment(min_epochs=3, max_epochs=5, batch_size=32):
     datamodule_type = DataModuleType.CIFAR10
     datamodule_hparams = {'batch_size': batch_size, 'data_dir': BASE_DATA_DIR}
 
     model_type = ModelType.VGG11
     model_hparams = {'num_classes': 10, 'num_channels': 3, 'bias': False}
     lightning_params = {'optimizer': 'sgd', 'lr': 0.1, 'momentum': 0.9, 'weight_decay': 0.0001, 'lr_scheduler': 'multistep', 'lr_decay_factor': 0.1, 'lr_decay_epochs': [150, 250]}
-    wandb_tags = ['RESNET-18', 'CIFAR_10', f"Batch size {batch_size}"]
+    wandb_tags = ['Loss', 'visualization', 'example']
 
     model, datamodule, trainer = setup_training('Saving plot for losviz', model_type, model_hparams, lightning_params, datamodule_type, datamodule_hparams, min_epochs=min_epochs, max_epochs=max_epochs, wandb_tags=wandb_tags)
 
@@ -28,7 +28,7 @@ def run_experiment(min_epochs=1, max_epochs=2, batch_size=256):
 
     wandb.finish()
 
-    save_path = r'C:\Users\filos\OneDrive\Desktop\ETH\loss-landscape\sidak\cifar10_vgg11_bs256.t7'
+    save_path = r'C:\Users\filos\OneDrive\Desktop\ETH\loss-landscape\sidak\cifar10_vgg11_bs32.t7'
 
     torch.save(model.state_dict(), save_path)
 
